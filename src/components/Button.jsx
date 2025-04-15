b02c8be0682b4225dde89929a87aa0e27b7313c6
Iman Langaran <imanlangaran@gmail.com>
2025-04-15 15:34:26 +0330

    finished hero

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,17 +1,32 @@
-import React from 'react'
+import React from "react";
 
-const Button = ({ className, id = '', text }) => {
-    return (
-        <a className={`${className ?? ''} cta-wrapper`} href={`#${id}`}>
-            <div className='cta-button group'>
-                <div className='bg-circle' />
-                <p className='text'>{text}</p>
-                <div className='arrow-wrapper'>
-                    <img src='/images/arrow-down.svg' alt='arrow' />
-                </div>
-            </div>
-        </a>
-    )
-}
+const Button = ({ className, id = "", text }) => {
+  return (
+    <a
+      onClick={(e) => {
+        e.preventDefault();
+        const target = document.getElementById("counter");
+        if (target && id) {
+          //   target.scrollIntoView({ behavior: "smooth" });
+          const offset = window.innerHeight * 0.15;
+          const top =
+            target.getBoundingClientRect().top + window.scrollY - offset;
 
-export default Button
+          window.scrollTo({ top, behavior: "smooth" });
+        }
+      }}
+      className={`${className ?? ""} cta-wrapper`}
+      href={`#${id}`}
+    >
+      <div className="cta-button group">
+        <div className="bg-circle" />
+        <p className="text">{text}</p>
+        <div className="arrow-wrapper">
+          <img src="/images/arrow-down.svg" alt="arrow" />
+        </div>
+      </div>
+    </a>
+  );
+};
+
+export default Button;
--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,8 +1,27 @@
-import Button from "../components/Button.jsx"
-import HeroExperience from "../components/HeroModels/HeroExperience.jsx"
-import { words } from "../constants/index.js"
+import AnimatedCounter from "../components/AnimatedCounter.jsx";
+import Button from "../components/Button.jsx";
+import HeroExperience from "../components/HeroModels/HeroExperience.jsx";
+import { words } from "../constants/index.js";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
 
 const Hero = () => {
+  useGSAP(() => {
+    gsap.fromTo(
+      ".hero-text h1",
+      {
+        y: 50,
+        opacity: 0,
+      },
+      {
+        y: 0,
+        opacity: 1,
+        stagger: 0.2,
+        duration: 1,
+        ease: "power2.inOut",
+      }
+    );
+  });
   return (
     <section id="hero" className="relative overflow-hidden">
       <div className="absolute top-0 left-0 z-10">
@@ -18,11 +37,15 @@ const Hero = () => {
                 <span className="slide">
                   <span className="wrapper">
                     {words.map((word) => (
-                      <span key={word.text} className="flex items-center md:gap-3 gap-1 pb-1">
+                      <span
+                        key={word.text}
+                        className="flex items-center md:gap-3 gap-1 pb-1"
+                      >
                         <img
                           src={word.imgPath}
                           alt={word.text}
-                          className="xl:size-12 md:size-10 size-7 md:p-2 p-1 rounded-full bg-white-50" />
+                          className="xl:size-12 md:size-10 size-7 md:p-2 p-1 rounded-full bg-white-50"
+                        />
                         <span>{word.text}</span>
                       </span>
                     ))}
@@ -33,12 +56,14 @@ const Hero = () => {
               <h1>that Deliver Results</h1>
             </div>
             <p className="text-white-50 md:text-xl relative z-10 pointer-events-none">
-              Hi, I'm Iman, a developer based in Mashhad, Iran, with a passion for code.
+              Hi, I'm Iman, a developer based in Mashhad, Iran, with a passion
+              for code.
             </p>
             <Button
               className="md:w-80 md:h-16 w-60 h-12"
               id="button"
-              text="See my Work" />
+              text="See my Work"
+            />
           </div>
         </header>
 
@@ -48,8 +73,10 @@ const Hero = () => {
           </div>
         </figure>
       </div>
-    </section>
-  )
-}
 
-export default Hero
+      <AnimatedCounter />
+    </section>
+  );
+};
+
+export default Hero;
