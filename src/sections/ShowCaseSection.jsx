fd38315dbac0879b0eab48ceff6f7ac183e5843d
Iman Langaran <imanlangaran@gmail.com>
2025-04-16 15:22:12 +0330

    completed show case section

--- src/sections/ShowCaseSection.jsx
+++ src/sections/ShowCaseSection.jsx
@@ -1,11 +1,55 @@
-import React from "react";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import React, { useRef } from "react";
+
+gsap.registerPlugin(ScrollTrigger);
 
 const ShowCaseSection = () => {
+  const sectionRef = useRef(null);
+  const project1Ref = useRef(null);
+  const project2Ref = useRef(null);
+  const project3Ref = useRef(null);
+
+  useGSAP(() => {
+    const projects = [
+      project1Ref.current,
+      project2Ref.current,
+      project3Ref.current,
+    ];
+
+    projects.forEach((card, index) => {
+      gsap.fromTo(
+        card,
+        {
+          y: 50,
+          opacity: 0,
+        },
+        {
+          y: 0,
+          opacity: 1,
+          duration: 1,
+          delay: 0.3 * (index + 1),
+          scrollTrigger: {
+            trigger: card,
+            start: "top bottom-=100",
+          },
+        }
+      );
+    });
+
+    gsap.fromTo(
+      sectionRef.current,
+      { opacity: 0 },
+      { opacity: 1, duration: 1.5 }
+    );
+  }, []);
+
   return (
-    <div id="work" className="app-showcase">
+    <section id="work" ref={sectionRef} className="app-showcase">
       <div className="w-full">
         <div className="showcaselayout">
-          <div className="first-project-wrapper">
+          <div className="first-project-wrapper" ref={project1Ref}>
             <div className="image-wrapper">
               <img src="/images/project1.png" alt="Ryde" />
             </div>
@@ -15,17 +59,32 @@ const ShowCaseSection = () => {
                 called Ryde
               </h2>
               <p className="text-white-50 md:text-xl">
-                An app built with React Native, Expo, & TailwindCS for a fast, user-friendly experience.
+                An app built with React Native, Expo, & TailwindCS for a fast,
+                user-friendly experience.
               </p>
             </div>
           </div>
 
-          <div>
-            
+          <div className="project-list-wrapper overflow-hidden">
+            <div className="project" ref={project2Ref}>
+              <div className="image-wrapper bg-[#ffefdb]">
+                <img
+                  src="/images/project2.png"
+                  alt="Library Management Platform"
+                />
+              </div>
+              <h2>Library Management Platform</h2>
+            </div>
+            <div className="project" ref={project3Ref}>
+              <div className="image-wrapper bg-[#ffefdb]">
+                <img src="/images/project3.png" alt="YC Directory" />
+              </div>
+              <h2>Yc Directory - A Startup Showcase App</h2>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </section>
   );
 };
 
