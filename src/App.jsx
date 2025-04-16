fa2d8c431c9db21b0e781b56f76a5961f5f96d87
Iman Langaran <imanlangaran@gmail.com>
2025-04-16 15:41:21 +0330

    implement navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Hero from "./sections/Hero";
 import ShowCaseSection from "./sections/ShowCaseSection";
+import NavBar from "./components/NavBar";
 
 const App = () => {
   return (
     <>
+      <NavBar />
       <Hero />
-      <ShowCaseSection /> 
+      <ShowCaseSection />
     </>
   );
 };
